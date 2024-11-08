3733bf177ad8b69597a2dbadc5a5455d7e774439
Avarnu <emanuelelevi@Emanueles-Air.lan>
2024-11-08 20:13:11 +0200

    Describe your changes here

---
+++ config.js
@@ -0,0 +1,13 @@
+{
+  "hostname": "Levi'stuff",
+  "apiKey": "ckey_01a42c27e2242a76035ff48eed4d",
+  "secret": "csec_ee205979e4ebf62f7d703ca498aaa3f4cc56e7fa938795a2",
+  "license": "This API key is provided free of charge. Use of this key or the API it accesses is at your own risk with no warranty or liability. By using this key you agree to properly attribute ALTCHA on your website as described in the documentation (https://altcha.org/docs/api/api_keys#attribution-for-free-api-keys). Terms of Service apply ((https://altcha.org/terms-of-service).",
+  "verification": {
+    "DNS": {
+      "name": "@",
+      "type": "TXT",
+      "value": "altcha-verification=2ba16a94a584b7e8"
+    }
+  }
+}
